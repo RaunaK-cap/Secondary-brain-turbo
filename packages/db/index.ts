3eb7ca1a0ae230cadd5bfed226d4f9dee0a5fa18
Raunak <raunakkumar6437@gmail.com>
2026-01-21 15:46:51 +0530

    db changes

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,13 +1,17 @@
-import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
+import { config } from "dotenv";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+config({ path: join(__dirname, ".env") });
+
 import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg'
 
 
 const connectionString = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient({ adapter }) 
 
 export { prisma }
-
-
